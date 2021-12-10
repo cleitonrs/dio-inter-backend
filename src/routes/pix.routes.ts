5a86f7798e501e95fa637f18bf477305043428fa
cleitonrs <rscleitonrs@gmail.com>
2021-12-10 16:58:47 -0300

    pix route created

---
+++ src/routes/pix.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const pixRouter = Router()
+// const pixController = new UserController()
+
+// pixRouter.post('/signin', pixController.signin)
+// pixRouter.post('/signup', pixController.signup)
+// pixRouter.get('/signup', pixController.signup)
+
+
+export default pixRouter
