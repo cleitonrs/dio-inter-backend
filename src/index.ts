9f8cdae500f37a97308a1de62fe96698922bb8c3
cleitonrs <rscleitonrs@gmail.com>
2021-12-10 16:30:21 -0300

    user service and user controllers done

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'express-async-errors'
 import express from 'express';
 import { createConnection } from 'typeorm';
 import { globalErrors } from './middlewares/globalErrors';
@@ -7,6 +8,7 @@ createConnection().then(connection => {
   const app = express();
   const PORT = 3333;
 
+  app.use(express.json())
   app.use(routes);
   app.use(globalErrors);
 
--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -1,7 +1,9 @@
 import { getRepository } from "typeorm";
 import md5 from "crypto-js/md5"
+import { sign } from "jsonwebtoken"
 
 import { User } from "../../entity/User";
+import authConfig from "../../config/auth"
 
 import { UserSignIn } from "./dtos/user.signin.dto";
 import { UserSignUp } from "./dtos/user.signup.dto";
@@ -19,11 +21,59 @@ export default class UserService {
       throw new AppError('Usuário não encontrado', 401)
     }
 
-    return existUser
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({
+      firstName: existUser.firstName,
+      lastName: existUser.lastName,
+      accountNumber: existUser.accountNumber,
+      accountDigit: existUser.accountDigit,
+      wallet: existUser.wallet
+    }, secret, {
+      subject: existUser.id,
+      expiresIn
+    })
+
+    // @ts-expect-error ignore
+    delete existUser.password
+
+    return {accessToken: token}
   }
 
 
   async signup(user: UserSignUp) {
+    const userRepository = getRepository(User)
+
+    const exitUser = await userRepository.findOne({where: {email: user.email}})
+    
+    if (exitUser) {
+      throw new AppError('Já existe um usuário cadastrado com esse e-mail', 401)
+    }
+
+    const userData = {
+      ...user,
+      password: md5(user.password).toString(),
+      wallet: 5000,
+      accountNumber: Math.floor(Math.random() * 999999),
+      accountDigit: Math.floor(Math.random() * 99)
+    }
+
+    const userCreate = await userRepository.save(userData)
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      accountNumber: userData.accountNumber,
+      accountDigit: userData.accountDigit,
+      wallet: userData.wallet
+    }, secret, {
+      subject: userCreate.id,
+      expiresIn
+    })
+
+    return {accessToken: token}
     
   }
 }
