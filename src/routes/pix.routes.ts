e163402c2c5b1537914e9ac0ade6765d51893e3f
cleitonrs <rscleitonrs@gmail.com>
2021-12-10 17:19:22 -0300

    resources pix done

--- src/routes/pix.routes.ts
+++ src/routes/pix.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import userAuthenticated from "../middlewares/userAuthenticated";
 
 const pixRouter = Router()
 // const pixController = new UserController()
+
+pixRouter.use(userAuthenticated)
 
 // pixRouter.post('/signin', pixController.signin)
 // pixRouter.post('/signup', pixController.signup)
